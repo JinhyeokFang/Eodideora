06efff43e99e0654a69220b8c2d10338e2e29051
JinhyeokBang <jinhyeokfang@gmail.com>
2023-11-02 13:39:03 +0900

    chore: init

---
+++ src/main/kotlin/com/example/eodideora/EodideoraApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eodideora
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EodideoraApplication
+
+fun main(args: Array<String>) {
+    runApplication<EodideoraApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/eodideora/EodideoraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.eodideora
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EodideoraApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
