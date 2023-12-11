2ae4d28701bec97ee4b2042911ee672638631cef
JinhyeokBang <jinhyeokfang@gmail.com>
2023-12-11 17:52:05 +0900

    feat: handling unauthorized request

--- src/main/kotlin/com/example/eodideora/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/eodideora/config/SecurityConfig.kt
@@ -5,15 +5,15 @@ import com.example.eodideora.auth.SuccessHandler
 import com.example.eodideora.util.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
-import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
-import org.springframework.web.server.ServerWebExchange
-import reactor.core.publisher.Mono
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint
+import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler
 
 @Configuration
 @EnableWebFluxSecurity
@@ -40,6 +40,12 @@ class SecurityConfig(
             }
             cors {
                 disable()
+            }
+            exceptionHandling {
+                authenticationEntryPoint = ServerAuthenticationEntryPoint { exchange, _ ->
+                    exchange.response.statusCode = HttpStatus.UNAUTHORIZED
+                    exchange.response.setComplete()
+                }
             }
             addFilterBefore(AccessTokenFilter(jwtProvider), SecurityWebFiltersOrder.AUTHENTICATION)
             oauth2Login {
